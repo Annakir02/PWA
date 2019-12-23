0b5066d5be4b6c99a94287986dd7467701949729
Anna Kirakosyan <annakirakosyan92@gmail.com>
2019-12-22 23:26:41 -0800

    Add product Carousel

---
+++ app/code/Yereone/ProductCarousel/Block/Widget/ProductCarousel.php
@@ -0,0 +1,9 @@
+<?php
+namespace Yereone\ProductCarousel\Block\Widget;
+
+use Magento\Widget\Block\BlockInterface;
+
+class ProductCarousel extends \Magento\CatalogWidget\Block\Product\ProductsList implements BlockInterface
+{
+
+}
---
+++ app/code/Yereone/ProductCarousel/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Yereone_ProductCarousel', __DIR__);
